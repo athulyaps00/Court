feffe24d9b4598940032438f379cb04486aa6ff0
athulyaps00 <134292791+athulyaps00@users.noreply.github.com>
2024-04-07 00:10:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+ // JavaScript source code
+    //<script src="https://kit.fontawesome.com/f8e1a90484.js" crossorigin="anonymous"></script>
+
+
+        let menuList = document.getElementById("menuList")
+        menuList.style.maxHeight = "0px";
+
+        function toggleMenu(){
+            if(menuList.style.maxHeight == "0px")
+            {
+                menuList.style.maxHeight = "300px";
+            }
+            else{
+                menuList.style.maxHeight = "0px";
+            }
+        }
+        
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
